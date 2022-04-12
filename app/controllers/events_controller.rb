1e3577e569085bf470a7d92f1840a3cc00bbfacd
BriannaLondon <briannajlondon@gmail.com>
2022-04-12 10:35:28 +0200

    index 2

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,15 @@
 class EventsController < ApplicationController
   def index
-
+    if params[:commit].present?
+      @events = policy_scope(Event).where(level: params[:level]).where(sport: params[:sport]).where(data: params[:data])
+    else
+      @events = policy_scope(Event)
+      puts "No available matches, try another search!"
+    end
   end
+
+  def show
+    @bookings = Booking.all
+  end
+
 end
