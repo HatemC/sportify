47e765ed2b2b402c33570cc7d34e366140005f8c
BriannaLondon <briannajlondon@gmail.com>
2022-04-13 09:00:41 +0200

    index with map and filter search

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  geocoded_by :address
 
   validates :sport, :level, :date, :duration, presence: true
 end
