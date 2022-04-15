201d685781b02b1a7bbbec322c925a3d32c16402
Hoi Yan Lam <iamhy10421@gmail.com>
2022-04-15 15:59:56 +0200

    css for dashboard & nav bar icon

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_one_attached :photo
   geocoded_by :address
 
   validates :sport, :level, :date, :duration, presence: true
