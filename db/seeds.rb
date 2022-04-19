c3565678f61694dc2cc1a8d927883924e5b75661
BriannaLondon <briannajlondon@gmail.com>
2022-04-19 16:46:58 +0200

    new seed and other small things

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
+Message.destroy_all
 Booking.destroy_all
 Event.destroy_all
 Chatroom.destroy_all
@@ -34,33 +35,63 @@ users_data['results'].each do |user_data|
   user.save!
 end
 
+puts "Creating 4 more users"
+alice = User.create!(first_name: 'Alice', last_name: 'Monet', address: '38 rue de verneuil paris', email: 'alice@gmail.com', password:'123456', bio: "I love playing sports")
+file = URI.open('http://ghost.skillshub.info/content/images/2017/01/profile-girl-square.png')
+alice.photo.attach(io: file, filename: 'alice.png', content_type: 'image/png')
+
+raoni = User.create!(first_name: 'Raoni', last_name: 'Pirat', address: '24 rue de sevres paris', email: 'jean@gmail.com', password:'123456', bio: "I love playing sports")
+file = URI.open('https://media-exp1.licdn.com/dms/image/C4E03AQFl3h14nxUEvg/profile-displayphoto-shrink_400_400/0/1641928856269?e=1655942400&v=beta&t=Exfjjx_eoYCb9YhVDnqhlZIIvfpDT31RBL2pYIreu4Q')
+raoni.photo.attach(io: file, filename: 'raoni.png', content_type: 'image/png')
+
+jennifer = User.create!(first_name: 'Jennifer', last_name: 'Carrow', address: '20 rue du store paris', email: 'jennifer@gmail.com', password:'123456', bio: "I love playing sports")
+file = URI.open('https://media-exp1.licdn.com/dms/image/C4D03AQGdUxB-dcF6KQ/profile-displayphoto-shrink_400_400/0/1641680245372?e=1655942400&v=beta&t=kNSBgg7eso-gZmSFbsrowSWdrAVslftndrTZ6BGXLZI')
+jennifer.photo.attach(io: file, filename: 'jennifer.png', content_type: 'image/png')
+
+brianna = User.create!(first_name: 'Brianna', last_name: 'London', address: '8 rue de verneuil paris', email: 'brianna@gmail.com', password:'123456', bio: "I love playing sports")
+file = URI.open('https://media-exp1.licdn.com/dms/image/C5603AQEE2MXahFNkaw/profile-displayphoto-shrink_400_400/0/1517474598541?e=1651708800&v=beta&t=XHmnp9CtlvlFdeb7Tgk-Kqu8rYgqQhSRjlT7VAmgtCU')
+brianna.photo.attach(io: file, filename: 'brianna.png', content_type: 'image/png')
+
 level = ['Beginner', 'Intermediate', 'Advanced']
 sport = ['Tennis', 'Football', 'Yoga', 'Soccer', 'Badminton', 'Running']
 duration = ['1 hour', '2 hour']
 event_name = ['Sport-acholic', 'Summer workout', 'Mindfulness with ME', 'Summer body get ready!']
+event_pic = ['https://cloudfront-us-east-1.images.arcpublishing.com/advancelocal/R7DQ3DK3SJDLNPIDGZHMSNQC6Y.jpeg', 'https://apexindoorsports.com/wp-content/uploads/2018/06/adultSports.jpg']
 
 
-puts 'Creating 20 events...'
+puts 'Creating 50 events...'
 
-20.times do
+alice_e1 = Event.new(event_name: "Women's Soccer", sport: 'Soccer', level: 'Beginner', date: Date.today+rand(90), duration: '1 hour')
+alice_e1.user = alice
+alice_e1.address = alice.address
+event_file = URI.open(event_pic.sample)
+alice_e1.photo.attach(io: event_file, filename: 'alice_e1.png', content_type: 'image/png')
+alice_e1.save!
+
+
+50.times do
   event = Event.new(
     user: User.all.sample,
     sport: sport.sample,
     level: level.sample,
     date: Date.today+rand(90),
     duration: duration.sample,
-
     address: address.sample + ", Paris, France",
-
     event_name: event_name.sample
-
   )
+
+  event_file = URI.open('https://lifestylefrisco.com/wp-content/uploads/2018/04/Frisco-Chamber-Field-Day-6.jpg')
+  event.photo.attach(io: event_file, filename: 'event.png', content_type: 'image/png')
+
   event.save!
+
 end
 
 20.times do
   chatroom = Chatroom.new(sender: User.all.sample, recepient: User.all.sample)
   chatroom.save!
 end
+
+
 
 puts "#{Event.count} events created"
